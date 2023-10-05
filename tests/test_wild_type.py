25484dc19e7c74582ee244cf389daa9920b29c9b
James Seager <james.seager@rothamsted.ac.uk>
2023-10-05 11:55:57 +0100

    Add test for is_metagenotype_wild_type

--- tests/test_wild_type.py
+++ tests/test_wild_type.py
@@ -1,6 +1,7 @@
 from phibase_pipeline.wild_type import (
     convert_allele_to_wild_type,
     is_genotype_wild_type,
+    is_metagenotype_wild_type,
 )
 
 
@@ -59,3 +60,42 @@ def test_is_genotype_wild_type():
     }
     assert is_genotype_wild_type(session, wt_genotype)
     assert not is_genotype_wild_type(session, mutant_genotype)
+
+
+def test_is_metagenotype_wild_type():
+    session = {
+        'alleles': {
+            'Q00000:0123456789abcdef-1': {
+                'allele_type': 'wild_type',
+            },
+            'Q00000:0123456789abcdef-2': {
+                'allele_type': 'deletion',
+            },
+        },
+        'genotypes': {
+            '0123456789abcdef-genotype-1': {
+                'loci': [
+                    [
+                        {
+                            'expression': 'Not assayed',
+                            'id': 'Q00000:0123456789abcdef-1',
+                        }
+                    ]
+                ]
+            },
+            '0123456789abcdef-genotype-2': {
+                'loci': [
+                    [
+                        {
+                            'expression': 'Not assayed',
+                            'id': 'Q00000:0123456789abcdef-2',
+                        }
+                    ]
+                ]
+            },
+        },
+    }
+    wt_metagenotype = {'pathogen_genotype': '0123456789abcdef-genotype-1'}
+    mutant_metagenotype = {'pathogen_genotype': '0123456789abcdef-genotype-2'}
+    assert is_metagenotype_wild_type(session, wt_metagenotype)
+    assert not is_metagenotype_wild_type(session, mutant_metagenotype)
