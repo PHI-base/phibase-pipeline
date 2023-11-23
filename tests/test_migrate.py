6970a8b45ade3efa0a4af3a21ce1a83971c6a76b
James Seager <james.seager@rothamsted.ac.uk>
2023-11-23 13:52:49 +0000

    Add test for load_exp_tech_mapping

--- tests/test_migrate.py
+++ tests/test_migrate.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 from phibase_pipeline.migrate import (
     load_bto_id_mapping,
+    load_exp_tech_mapping,
     load_phipo_mapping,
 )
 
@@ -30,3 +32,46 @@ def test_load_phipo_mapping():
     }
     actual = load_phipo_mapping(DATA_DIR / 'phipo.csv')
     assert actual == expected
+
+
+def test_load_exp_tech_mapping():
+    data = [
+        {
+            "exp_technique_stable": "AA substitution with overexpression",
+            "gene_protein_modification": "D144P-Y222A",
+            "exp_technique_transient": np.nan,
+            "allele_type": "amino acid substitution(s)",
+            "name": np.nan,
+            "description": "D144P-Y222A",
+            "expression": "Overexpression",
+            "evidence_code": "Unknown",
+            "eco": np.nan,
+        },
+        {
+            "exp_technique_stable": "AA substitution and deletion",
+            "gene_protein_modification": "ABCdelta, D144A-Y222A",
+            "exp_technique_transient": np.nan,
+            "allele_type": "amino acid substitution(s) | deletion",
+            "name": np.nan,
+            "description": "D144A,Y222A",
+            "expression": "Overexpression",
+            "evidence_code": "Unknown",
+            "eco": np.nan,
+        },
+        {
+            "exp_technique_stable": "duplicate row",
+            "gene_protein_modification": "D144A-Y222A",
+            "exp_technique_transient": np.nan,
+            "allele_type": "amino acid substitution(s)",
+            "name": np.nan,
+            "description": "D144A,Y222A",
+            "expression": "Overexpression",
+            "evidence_code": "Unknown",
+            "eco": np.nan,
+        },
+    ]
+    expected = pd.DataFrame.from_records(data)
+    actual = load_exp_tech_mapping(DATA_DIR / 'allele_mapping.csv')
+    assert actual.equals(expected), actual.compare(
+        expected, result_names=('actual', 'expected')
+    )
