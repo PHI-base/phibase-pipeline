948fa5ec378c952d9474ff28084a3946cb45741a
James Seager <james.seager@rothamsted.ac.uk>
2024-05-01 12:47:57 +0100

    Format postprocess.py

--- src/phibase_pipeline/postprocess.py
+++ src/phibase_pipeline/postprocess.py
@@ -252,7 +252,7 @@ def remove_allele_gene_names(session):
 
 
 def add_delta_symbol(session):
-    """ Replace 'delta' with the capital delta symbol in allele names. """
+    """Replace 'delta' with the capital delta symbol in allele names."""
     pattern = re.compile(r'delta$')
     DELTA = '\N{GREEK CAPITAL LETTER DELTA}'
     for allele in session.get('alleles', {}).values():
