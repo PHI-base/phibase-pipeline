1afaacdae3ee77f607acca1abf1a0cb075931a99
James Seager <james.seager@rothamsted.ac.uk>
2024-08-13 15:44:57 +0100

    Update rekey_duplicate_feature_ids
    Add more specific checks for duplicate features, and include duplicated
    features in the return value to make merging easier.

--- src/phibase_pipeline/merge.py
+++ src/phibase_pipeline/merge.py
@@ -64,20 +64,44 @@ def rekey_duplicate_feature_ids(feature_type, phibase_session, canto_session):
             set(features_b.keys())
         )
 
-    rekeyed_features = {}
+    def features_are_unique(feature_type, feature_a, feature_b):
+        match feature_type:
+            case 'alleles':
+                return (
+                    feature_a['gene'] != feature_b['gene']
+                    or feature_a['primary_identifier'] != feature_b['primary_identifier']
+                )
+            case 'genotypes':
+                return (
+                    feature_a['loci'] != feature_b['loci']
+                    or feature_a['organism_strain'] != feature_b['organism_strain']
+                    or feature_a['organism_taxonid'] != feature_b['organism_taxonid']
+                )
+            case 'metagenotypes':
+                return (
+                    feature_a['pathogen_genotype'] != feature_b['pathogen_genotype']
+                    or feature_a['host_genotype'] != feature_b['host_genotype']
+                )
+            case _:
+                raise ValueError(f'unsupported feature type: {feature_type}')
+
     exports = (phibase_session, canto_session)
     features = tuple(e[feature_type] for e in exports)
     next_feature_num = 1 + max(map(get_max_feature_number, features))
     features_a = phibase_session[feature_type]
     features_b = canto_session[feature_type]
     matching_ids = get_matching_feature_ids(features_a, features_b)
+    rekeyed_features = features_b.copy()
     for feature_id in matching_ids:
+        if 'wild-type' in feature_id:
+            continue
         feature_a = features_a[feature_id]
         feature_b = features_b[feature_id]
-        if (feature_a != feature_b) and 'wild-type' not in feature_id:
+        if features_are_unique(feature_type, feature_a, feature_b):
             feature_no_num = feature_id[:feature_id.rindex('-')]
             new_feature_id = f'{feature_no_num}-{next_feature_num}'
             rekeyed_features[new_feature_id] = feature_b
+            del rekeyed_features[feature_id]
             next_feature_num += 1
 
     return rekeyed_features
