d82beb80ed2a663c0e03b084b6fa19635d04ba1b
James Seager <james.seager@rothamsted.ac.uk>
2024-11-13 14:34:07 +0000

    Extract CLI functions to cli module

--- src/phibase_pipeline/__main__.py
+++ src/phibase_pipeline/__main__.py
@@ -1,77 +1,11 @@
-import argparse
-import json
+# SPDX-FileCopyrightText: 2023-present James Seager <james.seager@rothamsted.ac.uk>
+#
+# SPDX-License-Identifier: MIT
 
-from phibase_pipeline.ensembl import write_ensembl_exports
-from phibase_pipeline.migrate import make_combined_export
-from phibase_pipeline.validate import validate_export
+import sys
 
+from phibase_pipeline import cli
 
-parser = argparse.ArgumentParser(
-    prog='phibase-pipeline',
-    description='Pipeline for version 5 of the PHI-base database.',
-)
-subparsers = parser.add_subparsers(dest='target', required=True)
-
-parser_zenodo = subparsers.add_parser('zenodo')
-parser_zenodo.add_argument(
-    'phibase',
-    metavar='PHIBASE_CSV',
-    type=str,
-    help='the path to the PHI-base 4 CSV export file',
-)
-parser_zenodo.add_argument(
-    'phicanto',
-    metavar='CANTO_JSON',
-    type=str,
-    help='the path to the PHI-Canto JSON export file',
-)
-parser_zenodo.add_argument(
-    'output',
-    metavar='OUTFILE',
-    type=str,
-    help='the path to write the combined JSON export file',
-)
-
-parser_ensembl = subparsers.add_parser('ensembl')
-parser_ensembl.add_argument(
-    'phibase',
-    metavar='PHIBASE_CSV',
-    type=str,
-    help='the path to the PHI-base 4 CSV export file',
-)
-parser_ensembl.add_argument(
-    'phicanto',
-    metavar='CANTO_JSON',
-    type=str,
-    help='the path to the PHI-Canto JSON export file',
-)
-parser_ensembl.add_argument(
-    'uniprot_data',
-    metavar='UNIPROT_DATA',
-    type=str,
-    help='the path to the UniProt data file',
-)
-parser_ensembl.add_argument(
-    'out_dir',
-    metavar='DIR',
-    type=str,
-    help='the output directory for the Ensembl export files',
-)
-
-args = parser.parse_args()
-
-if args.target == 'zenodo':
-    canto_json = make_combined_export(args.phibase, args.phicanto)
-    validate_export(canto_json)
-    with open(args.output, 'w+', encoding='utf8') as json_file:
-        json.dump(canto_json, json_file, indent=4, sort_keys=True, ensure_ascii=False)
-elif args.target == 'ensembl':
-    write_ensembl_exports(
-        phibase_path=args.phibase,
-        canto_export_path=args.phicanto,
-        uniprot_data_path=args.uniprot_data,
-        dir_path=args.out_dir,
-    )
-else:
-    # argparse should prevent this from being reached
-    raise ValueError(f'unsupported target type: {args.target}')
+if __name__ == '__main__':
+    # First item of args is the script name: skip it
+    cli.run(sys.argv[1:])
---
+++ src/phibase_pipeline/cli.py
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: 2023-present James Seager <james.seager@rothamsted.ac.uk>
+#
+# SPDX-License-Identifier: MIT
+
+"""Command line interface for the phibase-pipeline package."""
+
+import argparse
+import json
+
+from phibase_pipeline import ensembl, migrate, validate
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        prog='phibase-pipeline',
+        description='Pipeline for version 5 of the PHI-base database.',
+    )
+    subparsers = parser.add_subparsers(dest='target', required=True)
+    parser_zenodo = subparsers.add_parser('zenodo')
+    parser_ensembl = subparsers.add_parser('ensembl')
+
+    shared_args = {
+        'phibase': {
+            'metavar': 'PHIBASE_CSV',
+            'type': str,
+            'help': 'the path to the PHI-base 4 CSV export file',
+        },
+        'phicanto': {
+            'metavar': 'CANTO_JSON',
+            'type': str,
+            'help': 'the path to the PHI-Canto JSON export file',
+        }
+    }
+    for subparser in (parser_zenodo, parser_ensembl):
+        for arg_name, kwargs in shared_args.items():
+            subparser.add_argument(arg_name, **kwargs)
+
+    parser_zenodo.add_argument(
+        'output',
+        metavar='OUTFILE',
+        type=str,
+        help='the path to write the combined JSON export file',
+    )
+
+    parser_ensembl.add_argument(
+        'uniprot_data',
+        metavar='UNIPROT_DATA',
+        type=str,
+        help='the path to the UniProt data file',
+    )
+    parser_ensembl.add_argument(
+        'out_dir',
+        metavar='DIR',
+        type=str,
+        help='the output directory for the Ensembl export files',
+    )
+    return parser.parse_args(args)
+
+
+def run(args):
+    args = parse_args(args)
+    if args.target == 'zenodo':
+        canto_json = migrate.make_combined_export(args.phibase, args.phicanto)
+        validate.validate_export(canto_json)
+        with open(args.output, 'w+', encoding='utf8') as json_file:
+            json.dump(canto_json, json_file, indent=4, sort_keys=True, ensure_ascii=False)
+    elif args.target == 'ensembl':
+        ensembl.write_ensembl_exports(
+            phibase_path=args.phibase,
+            canto_export_path=args.phicanto,
+            uniprot_data_path=args.uniprot_data,
+            dir_path=args.out_dir,
+        )
+    else:
+        # argparse should prevent this from being reached
+        raise ValueError(f'unsupported target type: {args.target}')
