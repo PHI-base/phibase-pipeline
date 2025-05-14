6fdb9e94bd7b8a611316aa94e8d3ef13e1a1885d
James Seager <james.seager@rothamsted.ac.uk>
2025-05-14 13:48:30 +0100

    Test extracting labels from multiple ontologies

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -8,15 +8,18 @@ from phibase_pipeline.robot import get_ontology_terms_and_labels
 
 
 TEST_DATA_DIR = Path(__file__).parent / 'data'
+ROBOT_DATA_DIR = TEST_DATA_DIR / 'robot'
 
 
 def test_get_ontology_terms_and_labels():
     expected = {
         'TEST:0000001': 'term 1',
         'TEST:0000002': 'term 2',
+        'TEST2:0000001': 'term 3',
     }
     term_label_mapping_config = {
-        'TEST': TEST_DATA_DIR / 'test_ontology.owl',
+        'TEST': ROBOT_DATA_DIR / 'test_ontology_1.owl',
+        'TEST2': ROBOT_DATA_DIR / 'test_ontology_2.owl',
     }
     actual = get_ontology_terms_and_labels(term_label_mapping_config)
     assert actual == expected
