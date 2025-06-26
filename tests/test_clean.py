e0e1dd9be2a789b6dd74c0f38a91d3aa81d23afd
James Seager <james.seager@rothamsted.ac.uk>
2025-06-26 14:56:42 +0100

    Add test for clean_phibase_csv

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -2,12 +2,17 @@
 #
 # SPDX-License-Identifier: MIT
 
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 import pytest
-from pandas.testing import assert_series_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 
-from phibase_pipeline.clean import fix_curation_dates
+from phibase_pipeline.clean import clean_phibase_csv, fix_curation_dates
+
+
+TEST_DATA_DIR = Path(__file__).parent / 'data'
 
 
 @pytest.mark.parametrize(
@@ -61,3 +66,22 @@ from phibase_pipeline.clean import fix_curation_dates
 def test_fix_curation_dates(dates, expected):
     actual = fix_curation_dates(dates)
     assert_series_equal(expected, actual)
+
+
+def test_clean_phibase_csv():
+    csv_path = TEST_DATA_DIR / 'clean' / 'clean_phibase_csv.csv'
+    expected_path = TEST_DATA_DIR / 'clean' / 'clean_phibase_csv_expected.csv'
+    expected = pd.read_csv(expected_path)
+    actual = clean_phibase_csv(csv_path)
+    # Fix a comparison error where actual dates have times, but expected ones don't.
+    expected['curation_date'] = pd.to_datetime(expected['curation_date']).dt.normalize()
+    expected = expected.astype(
+        {
+            'pathogen_id': 'UInt64',
+            'pathogen_strain_id': 'UInt64',
+            'host_id': 'UInt64',
+            'pmid': 'UInt64',
+            'year': 'UInt64',
+        }
+    )
+    assert_frame_equal(actual, expected)
