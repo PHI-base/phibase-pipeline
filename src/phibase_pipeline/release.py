b8fd35a5480113dcec4900712a12b03d76f666c3
James Seager <james.seager@rothamsted.ac.uk>
2025-08-05 15:12:54 +0100

    Add get_release_stats_table function

--- src/phibase_pipeline/release.py
+++ src/phibase_pipeline/release.py
@@ -9,7 +9,78 @@ or for uploading to the Zenodo repository or Ensembl.
 
 import json
 
+import pandas as pd
+
 from phibase_pipeline import ensembl, loaders, migrate, postprocess, robot, spreadsheet, validate
+
+
+def get_release_stats_table(export):
+    def iter_feature_keys(feature_key):
+        for session in export['curation_sessions'].values():
+            features = session.get(feature_key, {})
+            for feature_id in features:
+                yield feature_id
+
+    def iter_taxon_ids(role):
+        for session in export['curation_sessions'].values():
+            for taxon_id, organism in session['organisms'].items():
+                if organism['role'] == role:
+                    yield taxon_id
+
+    def count_unique_annotations(annotation_type):
+        return len(set(
+            annotation['term']
+            for session in export['curation_sessions'].values()
+            for annotation in session['annotations']
+            if annotation['type'] == annotation_type
+        ))
+
+    def count_annotations(annotation_type):
+        return len(list(
+            annotation
+            for session in export['curation_sessions'].values()
+            for annotation in session['annotations']
+            if annotation['type'] == annotation_type
+        ))
+
+    count_features = lambda k: len(set(iter_feature_keys(k)))
+
+    annotation_types = {
+        'Pathogen-host interaction phenotype': 'pathogen_host_interaction_phenotype',
+        'Gene-for-gene phenotype': 'gene_for_gene_phenotype',
+        'Pathogen phenotype': 'pathogen_phenotype',
+        'Host phenotype': 'host_phenotype',
+        'GO biological process': 'biological_process',
+        'GO cellular component': 'cellular_component',
+        'GO molecular function': 'molecular_function',
+        'Post-translational modification': 'post_translational_modification',
+        'Physical interaction': 'physical_interaction',
+        'WT RNA expression': 'wt_rna_expression',
+        'WT protein expression': 'wt_protein_expression',
+    }
+    annotation_counts = {
+        label: count_annotations(a_type) if type(a_type) == str else a_type
+        for label, a_type in annotation_types.items()
+    }
+    data_stats = {
+        'Genes': count_features('genes'),
+        'Interactions': count_features('metagenotypes'),
+        'Pathogen species': len(set(iter_taxon_ids('pathogen'))),
+        'Host species': len(set(iter_taxon_ids('host'))),
+        'Diseases': count_unique_annotations('disease_name'),
+        'References': count_features('publications'),
+        **annotation_counts,
+    }
+    stats_df = (
+        pd.DataFrame(
+            data_stats,
+            columns=list(data_stats.keys()),
+            index=['Count']
+        )
+        .transpose()
+        .rename_axis('Data type')
+    )
+    return stats_df
 
 
 def write_json_export(path, canto_json):
